254731d69e1e759564dc4d70ac4ee61b1216e632
Derrick Kamphaus <Derrick.Kamphaus@pcmsgroup.com>
2023-04-18 08:39:14 -0400

    Initial commit

---
+++ CodeGen.py
@@ -0,0 +1,242 @@
+# This is a sample Python script.
+
+import string
+import pyperclip
+import io
+import argparse
+
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def parser_hook(text):
+    parsed_text = []
+    for line in text:
+        if '$include="' in line:
+            parsed_text.append(include(line))
+
+        if not ("#" in line):
+                parsed_text.append(line)
+
+    return ''.join(parsed_text)
+
+
+def include(line):
+    parts = line.split('"')
+    file = parts[1]
+    print(file)
+    file_text = open_file_template(file)
+    newLine = line.replace(file_text)
+    return newLine
+
+
+def outfile(file, text):
+    f = open(file, 'w')
+    f.write(text)
+    f.close()
+
+
+def open_file_list(fname):
+    with open(fname, 'r') as f:
+        lines = []
+        for line in f:
+            line = line.strip()
+            lines.append(line)
+    f.close()
+    return lines
+
+
+def clip_to_list():
+    lines = []
+    for line in io.StringIO(pyperclip.paste()):
+        line = line.strip()
+        lines.append(line)
+    return lines
+
+
+def open_file_template(fname):
+    f = open(fname, 'r')
+    text = f.read()
+    f.close()
+    return text
+
+
+def gen_num(numlist, data, inc):
+    newdata = data
+    for i in range(len(numlist)):
+        x = numlist[i]
+        xstr = str(x)
+        x += inc
+        numlist[i] = x
+        rep = '$n' + str(i + 1)
+
+        newdata = newdata.replace(rep, xstr)
+    return newdata
+
+
+def gen_number(numlist, data, loop, inc):
+    newlist = ""
+    for i in range(loop):
+        getline = gen_num(numlist, data, inc)
+        newlist += getline + "\n"
+
+    return newlist
+
+
+def multi_data(data, list):
+    lines = list.split("|")
+    i = 1
+    curData = data
+    for ln in lines:
+        rep = '$data' + str(i)
+        repData = curData.replace(rep, ln)
+        curData = repData
+        i += 1
+    return curData
+
+
+def single_data(data, line):
+    newdata = data.replace('$data.end', get_end_data(line))
+    newdata = newdata.replace('$data', line)
+    return newdata
+
+
+def get_data_path(line):
+    splitLines = line.split("/")
+    filename = splitLines[len(splitLines) - 1]
+
+    i = 1
+    path = ""
+    for l in splitLines:
+        if i > 3:
+            path += l
+            if i != len(splitLines):
+                path += '/'
+        i += 1
+    return path
+
+
+def get_end_data(line):
+    path = get_data_path(line)
+    splitPath = path.split('/')
+    fileName = splitPath[len(splitPath) - 1]
+    name = fileName.split(".")[0]
+    return name
+
+
+def gen_File_Arg(args):
+    listfile = args.file
+    exclude_file = args.exclude
+    datasource = args.source
+    multi = args.multi
+    numbers = args.number
+    pData = args.data
+    add_line = args.line
+    new_line = "\n"
+    out = args.out
+    onlynum = args.onlynum
+    inc = args.inc
+    seperate = args.seperate
+
+    if not inc:
+        inc = 1
+        print("inc default:")
+
+    data = open_file_template(datasource)
+    data = parser_hook(data)
+    newdata = data
+    newlines = ""
+    numlist = numbers
+    if (add_line):
+        new_line += "\n"
+
+
+
+    if not onlynum:
+        if listfile:
+            getlines = open_file_list(listfile)
+            print("Open File")
+
+        else:
+            getlines = clip_to_list()
+            print("Open from Clipboard")
+
+        for line in getlines:
+            # verify it isnt blank or contains a comment
+            if (line and line.strip()) and not("#" in line):
+
+                if exclude_file:
+                    exclude_lines = open_file_list(exclude_file)
+                    adjustedLine = line.split('/')
+                    if adjustedLine[len(adjustedLine)-1] in exclude_lines:
+                        continue
+
+                if multi:
+                    newdata = multi_data(data, line)
+
+                else:
+                    newdata = single_data(data, line)
+
+
+                if pData:
+                    i = 1
+                    for p in pData:
+                        rep = '$p' + str(i)
+                        repData = newdata.replace(rep, p)
+                        newdata = repData
+
+                if numbers:
+                    newdata = gen_num(numlist, newdata, inc)
+
+                if seperate:
+                    path = get_data_path(line)
+                    filepath = out + path
+                    outfile(filepath, newdata)
+                newlines += newdata + new_line
+
+    else:
+        # for x in range(int(onlynum)):
+        newdata = gen_number(numbers, data, onlynum, inc)
+        newlines += newdata
+    if listfile:
+        fileout = listfile + ".out"
+        if out:
+            fileout = out
+        if not seperate:
+            outfile(fileout, newlines)
+    else:
+        pyperclip.copy(newlines)
+
+
+if __name__ == '__main__':
+    ##file = 'test.txt'
+    ##remove_file(file + 'out')
+    ##open_file_change(file)
+    # total arguments
+    parser = argparse.ArgumentParser("Code Generator")
+    parser.add_argument("source", help="File containing data to surround your text list")
+    parser.add_argument("-f", "--file", help="gets input from a file then outputs to a file")
+    parser.add_argument("-e", "--exclude", help="gets a list from file to exclude items from the first list/clipboard")
+    parser.add_argument("-m", "--multi", help="Splits input list into multiple delimitated items", action="store_true")
+    parser.add_argument("-s", "--seperate", help="output into seperate files  expects $data1 to be path",
+                        action="store_true")
+    parser.add_argument("-o", "--out", help="sets the name of the output file")
+    parser.add_argument("-n", "--number", nargs="*", type=int, help="Increments number, with $n1, $n2 starting num",
+                        default=0)
+    parser.add_argument("-d", "--data", nargs="*", type=str, help="Additional data: $p1, $p2 ...")
+    parser.add_argument("-i", "--inc", type=int, help="Custom increment for numbers default is 1:", default=1)
+    parser.add_argument("--onlynum", type=int, help="uses no source, instead only increments numbers")
+    parser.add_argument("-l", "--line", help="Adds a new line after every item.", action="store_true")
+    args = parser.parse_args()
+
+    if args.file:
+        print("Using your file: {} , source is {}, numbers are {}".format(args.file, args.source, args.number))
+    if args.line:
+        print("Adding new line after each item.")
+    if args.multi:
+        print("Splitting input into deliminated items.")
+
+    elif not args.file:
+        print("Using clipboard")
+
+    gen_File_Arg(args)
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ Readme.txt
@@ -0,0 +1,79 @@
+----------READ ME -----------
+CodeGen.py
+Created by Derrick Kamphaus
+
+This script automates making repetitive code. It takes a source file / clipboard list, which is a line by line list
+that has the data which you need to wrap code around. Supply a template file to format around the code.
+
+Parameters / arguments:
+    source: Required baseline argument. This is the name of the file that will be used as the template
+--file -f : Specifies an input file to use as source for list. will output the same name but out
+    IE: CodeGen.py "myTemplate.txt" --f "Mylist.txt"
+    will pull "Mylist.txt" and use each line to wrap the template text from "myTemplate.txt" and output as Mylist.txt.out
+    not supplying file with this flag will default to clipboard as source and destination.
+
+--multi -m : Allows a supplied list to have multiple items per line deliminated by | each line will be parsed by the
+    template as $data1, $data2, ... $dataN , when not supplied template will be interpreted as $data
+
+--out -o : sets a specific name for output file, otherwise will use the name of the inputfile + .out
+
+--number -n : (any number of args)  Increments numbers in template $n1, $n2 ... $nX where n is the number at which it
+starts. --n "1 2 3" would increment 1 2 3 -> 2 3 4 -> 3 4 5 for each line in the source.
+
+--line -l : Adds a new line after each chunk iteration of the code wrap.
+
+--onlynum : increments numbers only and does not use a list source. Supply number of loops, and generates that many number
+of template based items with the supplied -n number list
+
+
+to use clipboard as the source of list and output of template: just supply the source and don't use --f
+CodeGen.py "template.txt"
+or other arguments: CodeGen.py "template.txt" -l -n "1 3 4"
+
+
+Example uses: XML1-, when you need to create multiple products where the only variance is the sku, name, and document number
+AV- when you have several files to make into includes: have a list of file1, file2 ,file3 ... simply write the list in the .test where you want it.
+supply source file with: <include path="$data"/>
+copy run CodeGen and paste a list of <include path="file1"/> .. <include path="fileX"/>
+
+
+Creating Template: create a file that has the text that will wrap around each data item supplied from the source list.
+variables: $data - whatever data from list source either file or clipboard will supply each line and replace with data
+$data1, $data2 .. $dataN see --multi flag
+
+$n1, $n2, $n3 ... - when --numbers is set, allows incrementing numbers to be replaced. Good for xml1s where multiple
+numbers may increment. each number can start from a different number.
+
+EX Template:
+my.tmpl
+
+<text>
+    <foo="$data"/>
+    <num="$n1">
+    <otherNum="$n2">
+</text>
+
+supplying the list of :
+thing1
+thing2
+thing3
+
+--numbers 1 4
+
+output would be:
+
+<text>
+    <foo="thing1"/>
+    <num="1">
+    <otherNum="4">
+</text>
+<text>
+    <foo="thing2"/>
+    <num="2">
+    <otherNum="5">
+</text>
+<text>
+    <foo="thing3"/>
+    <num="3">
+    <otherNum="6">
+</text>
